7d58615927bc181c170b3b37653722266bafb373
hawm <20796940+hawm@users.noreply.github.com>
2021-07-24 22:47:05 +0800

    reuse connection

--- mediawiki/settings/Cache.php
+++ mediawiki/settings/Cache.php
@@ -1,12 +1,12 @@
 <?php
 $wgObjectCaches['redis'] = [
-    'class'                => 'RedisBagOStuff',
-    'servers'              => [ 'cache-redis:6379' ],
-    'persistent'        => false,
+    'class' => 'RedisBagOStuff',
+    'servers' => ['cache-redis:6379'],
+    'persistent' => true,
 ];
 $wgMainCacheType = 'redis';
 $wgSessionCacheType = 'redis';
 $wgMessageCacheType = 'redis';
 $wgParserCacheType = 'redis';
 $wgLanguageConverterCacheType = 'redis';
-$wgCacheDirectory="$IP/cache";
+$wgCacheDirectory = "$IP/cache";
