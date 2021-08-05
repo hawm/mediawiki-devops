13279c01f6f97af6959feffab65c24960e7f180e
hawm <20796940+hawm@users.noreply.github.com>
2021-08-05 20:23:17 +0800

    make server cnofigurable

--- mediawiki/settings/commons/Database.php
+++ mediawiki/settings/commons/Database.php
@@ -1,7 +1,7 @@
 <?php
 ## Database settings
 $wgDBtype = "mysql";
-$wgDBserver = "db-mariadb";
+$wgDBserver = getenv('WIKI_DATABASE_HOST');
 $wgDBname = getenv('WIKI_DATABASE_NAME');
 $wgDBuser = getenv('WIKI_DATABASE_USER');
 $wgDBpassword = getenv('WIKI_DATABASE_PASSWORD');
