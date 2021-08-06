b703f0dece09599ed1b9324a9e72ad420f792bd1
hawm <20796940+hawm@users.noreply.github.com>
2021-08-06 17:12:50 +0800

    improve talk config

--- mediawiki/settings/commons/Talk.php
+++ mediawiki/settings/commons/Talk.php
@@ -1,17 +1,26 @@
 <?php
 
+$talkNS = [
+	NS_TALK,
+	NS_USER_TALK,
+	NS_PROJECT_TALK,
+	NS_FILE_TALK,
+	NS_MEDIAWIKI_TALK,
+	NS_TEMPLATE_TALK,
+	NS_HELP_TALK,
+	NS_CATEGORY_TALK,
+];
+
 # Disable all core TALK namespaces
-$wgNamespaceProtection[NS_TALK] = ['nobody'];
-$wgNamespaceProtection[NS_USER_TALK] = ['nobody'];
-$wgNamespaceProtection[NS_PROJECT_TALK] = ['nobody'];
-$wgNamespaceProtection[NS_FILE_TALK] = ['nobody'];
-$wgNamespaceProtection[NS_MEDIAWIKI_TALK] = ['nobody'];
-$wgNamespaceProtection[NS_TEMPLATE_TALK] = ['nobody'];
-$wgNamespaceProtection[NS_HELP_TALK] = ['nobody'];
-$wgNamespaceProtection[NS_CATEGORY_TALK] = ['nobody'];
-
+foreach ($talkNS as $ns) {
+	$wgNamespaceProtection[$ns] = ['nobody'];
+}
+# Remove tab/link
 $wgHooks['SkinTemplateNavigation::Universal'][] = function ($template, &$links) {
-	unset($links['namespaces']['talk']);
 	unset($links['user-menu']['mytalk']);
+	array_walk($links['namespaces'], function ($v, $k) use (&$links) {
+		if (preg_match('/[Tt]alk$/', $k)) {
+			unset($links['namespaces'][$k]);
+		}
+	});
 };
-
