416087995aa6bf1614188c047e932f994e22af84
hawm <20796940+hawm@users.noreply.github.com>
2021-08-07 16:46:43 +0800

    update licenses url to chinese version

--- mediawiki/settings/commons/Right.php
+++ mediawiki/settings/commons/Right.php
@@ -3,6 +3,6 @@
 ## appropriate copyright notice / icon. GNU Free Documentation
 ## License and Creative Commons licenses are supported so far.
 $wgRightsPage = ""; # Set to the title of a wiki page that describes your license/copyright
-$wgRightsUrl = "https://creativecommons.org/licenses/by-nc-sa/4.0/";
+$wgRightsUrl = "https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh";
 $wgRightsText = "知识共享署名-非商业性使用-相同方式共享";
 $wgRightsIcon = "$wgResourceBasePath/resources/assets/licenses/cc-by-nc-sa.png";
