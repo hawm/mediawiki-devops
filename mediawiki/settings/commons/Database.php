499c3342cc31ba127ca207b4d8ce0d94a8872d8f
hawm <20796940+hawm@users.noreply.github.com>
2021-08-08 16:53:49 +0800

    add db admin

--- mediawiki/settings/commons/Database.php
+++ mediawiki/settings/commons/Database.php
@@ -5,6 +5,7 @@ $wgDBserver = getenv('WIKI_DATABASE_HOST');
 $wgDBname = getenv('WIKI_DATABASE_NAME');
 $wgDBuser = getenv('WIKI_DATABASE_USER');
 $wgDBpassword = getenv('WIKI_DATABASE_PASSWORD');
-
+$wgDBadminuser = $wgDBuser;
+$wgDBadminpassword = $wgDBpassword;
 # MySQL specific settings
 $wgDBprefix = "";
