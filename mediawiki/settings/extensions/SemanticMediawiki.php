365fc4e8b9c544b9a13979fbcfd478084cd4b179
hawm <20796940+hawm@users.noreply.github.com>
2021-08-08 16:58:03 +0800

    enable semantic mediawiki

---
+++ mediawiki/settings/extensions/SemanticMediawiki.php
@@ -0,0 +1,5 @@
+<?php
+# Ignore deprecation
+# see https://github.com/SemanticMediaWiki/SemanticMediaWiki/issues?q=is%3Aissue+is%3Aopen+Deprecated
+$wgDeprecationReleaseLimit = '1.30';
+enableSemantics(getenv('WIKI_HOST'));
