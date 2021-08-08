f89e9a0d83f41726e3c97516c44ff44d4715ad89
hawm <20796940+hawm@users.noreply.github.com>
2021-08-08 16:56:59 +0800

    deliver job to redis

---
+++ mediawiki/settings/commons/Job.php
@@ -0,0 +1,9 @@
+<?php
+
+$wgJobTypeConf['default'] = [
+    'class'          => 'JobQueueRedis',
+    'redisServer'    => 'db-redis:6379',
+    'redisConfig'    => [],
+    'claimTTL'       => 3600,
+    'daemonized'     => true
+ ];
