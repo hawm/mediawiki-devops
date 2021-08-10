6a145efa59617c71ef46bf87219815639f51da7e
hawm <20796940+hawm@users.noreply.github.com>
2021-08-10 15:49:59 +0800

    improve extension installation

--- mediawiki/install_extensions.sh
+++ mediawiki/install_extensions.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+# Install extension by Composer
+composer update --no-dev
+# Install extension by git
 git clone --depth 1 --single-branch --branch REL1_36 \
     $GITHUB_MIRROR/wikimedia/mediawiki-extensions-Lockdown.git \
     $MW_INSTALL_PATH/extensions/Lockdown
