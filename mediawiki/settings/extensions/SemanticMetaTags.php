f8827323867108b3f77a7767ed21ad41c6180380
hawm <20796940+hawm@users.noreply.github.com>
2021-08-13 15:51:43 +0800

    enable extensions

---
+++ mediawiki/settings/extensions/SemanticMetaTags.php
@@ -0,0 +1,16 @@
+<?php
+
+wfLoadExtension('SemanticMetaTags');
+
+$GLOBALS['smtgTagsProperties'] = [
+	'description' => '简介',
+];
+
+$GLOBALS['smtgTagsStrings'] = [
+	// Static content tag
+];
+
+$GLOBALS['smtgMetaPropertyPrefixes'] = [ 
+	// Open Graph prefixes
+	'og:'
+];
