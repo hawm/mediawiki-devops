0b9a157ef1c53b31b28382a5f14c04957584aca0
hawm <20796940+hawm@users.noreply.github.com>
2021-08-18 14:34:09 +0800

    new debug config

--- mediawiki/settings/debugs/Cache.php
+++ mediawiki/settings/debugs/Cache.php
@@ -1,8 +1,10 @@
 <?php
 
-$wgCachePages = false;
-$wgMainCacheType = CACHE_NONE;
-$wgSessionCacheType = 'redis';
-$wgMessageCacheType = $wgMainCacheType;
-$wgParserCacheType = $wgMainCacheType;
-$wgLanguageConverterCacheType = $wgMainCacheType;
+if (strtolower(getenv('WIKI_DEBUG_DISABLE_CACHE')) === 'true'){
+    $wgCachePages = false;
+    $wgMainCacheType = CACHE_NONE;
+    $wgSessionCacheType = 'redis';
+    $wgMessageCacheType = $wgMainCacheType;
+    $wgParserCacheType = $wgMainCacheType;
+    $wgLanguageConverterCacheType = $wgMainCacheType;
+}
