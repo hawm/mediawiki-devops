d3ceddad65d99d7ca378683b23ef9a513107fd8a
hawm <20796940+hawm@users.noreply.github.com>
2021-09-11 23:20:00 +0800

    enable cache for smw

--- mediawiki/settings/extensions/SemanticMediawiki.php
+++ mediawiki/settings/extensions/SemanticMediawiki.php
@@ -3,3 +3,5 @@
 # see https://github.com/SemanticMediaWiki/SemanticMediaWiki/issues?q=is%3Aissue+is%3Aopen+Deprecated
 $wgDeprecationReleaseLimit = '1.30';
 enableSemantics(getenv('WIKI_HOST'));
+$smwgMainCacheType = 'redis';
+$smwgQueryResultCacheType = 'redis';
