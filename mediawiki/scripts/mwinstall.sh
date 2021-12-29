4f14281ab7e55ba212ea517e8e254950bdd86296
hawm <20796940+hawm@users.noreply.github.com>
2021-12-30 00:15:55 +0800

    update mariadb and redis name in env

--- mediawiki/scripts/mwinstall.sh
+++ mediawiki/scripts/mwinstall.sh
@@ -6,10 +6,10 @@ read -r -s -p "Admin Password: " password
 echo
 php "${MW_INSTALL_PATH}/maintenance/install.php" \
     --scriptpath=/ \
-    --dbserver="${WIKI_DATABASE_HOST}" \
-    --dbname="${WIKI_DATABASE_NAME}" \
-    --dbuser="${WIKI_DATABASE_USER}" \
-    --dbpass="${WIKI_DATABASE_PASSWORD}" \
+    --dbserver="${MARIADB_HOST}" \
+    --dbname="${MARIADB_DATABASE_NAME}" \
+    --dbuser="${MARIADB_DATABASE_USER}" \
+    --dbpass="${MARIADB_DATABASE_PASSWORD}" \
     --pass="$password" \
     TestWiki \
     "$username"
--- mediawiki/settings/commons/Database.php
+++ mediawiki/settings/commons/Database.php
@@ -1,10 +1,10 @@
 <?php
 ## Database settings
 $wgDBtype = "mysql";
-$wgDBserver = getenv('WIKI_DATABASE_HOST').':'.getenv('WIKI_DATABASE_PORT');
-$wgDBname = getenv('WIKI_DATABASE_NAME');
-$wgDBuser = getenv('WIKI_DATABASE_USER');
-$wgDBpassword = getenv('WIKI_DATABASE_PASSWORD');
+$wgDBserver = getenv('MARIADB_HOST').':'.getenv('MARIADB_PORT');
+$wgDBname = getenv('MARIADB_DATABASE_NAME');
+$wgDBuser = getenv('MARIADB_DATABASE_USER');
+$wgDBpassword = getenv('MARIADB_DATABASE_PASSWORD');
 $wgDBadminuser = $wgDBuser;
 $wgDBadminpassword = $wgDBpassword;
 # MySQL specific settings
